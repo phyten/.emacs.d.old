cc37e6019c1322ed705fbba508efda7f7ae2db19
phyten <phyten.obr@gmail.com>
2013-08-01 15:44:07 +0900

    フルスクリーン関連

--- inits/10_window.el
+++ inits/10_window.el
@@ -9,3 +9,17 @@
 
 ;; windowを分けても折り返す
 (setq truncate-partial-width-windows nil)
+
+;;画面最大化
+(defun mac-toggle-max-window ()
+  (interactive)
+  (if (frame-parameter nil 'fullscreen)
+      (set-frame-parameter nil 'fullscreen nil)
+    (set-frame-parameter nil 'fullscreen 'fullboth)))
+(setq mac-autohide-menubar-on-maximize nil)
+
+(custom-set-variables
+ '(display-time-mode t)
+ '(tool-bar-mode nil)
+ '(transient-mark-mode t))
+(custom-set-faces)
