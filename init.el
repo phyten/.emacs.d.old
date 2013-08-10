d9e54da35622d2737dc56245ed2e272f4bbeeb77
phyten <phyten.obr@gmail.com>
2013-08-11 07:06:58 +0900

    change theme

--- init.el
+++ init.el
@@ -1,7 +1,30 @@
 Local:    master ~/Dropbox/development/.emacs.d/
-Head:     8d1cd14 syntax highlight
+Head:     5f09d6d もろもろ変更
 
-Unstaged changes:
-	Modified   init.el
-	Modified   inits/60_ruby.el
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(ansi-term-color-vector [unspecific "#586e75" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#002b36"])
+ '(custom-enabled-themes (quote (apribase)))
+ '(custom-safe-themes (quote ("d0654aebee372ea638775f4f9f64b3e80ea0462dfd4388637b798142ba5020d0" "9be47486fd092ecefbc2bff25de4025cb4543d248764b4a42892415e759ec72f" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" "e9a1226ffed627ec58294d77c62aa9561ec5f42309a1f7a2423c6227e34e3581" "211bb9b24001d066a646809727efb9c9a2665c270c753aa125bace5e899cb523" default)))
+ '(display-time-mode t)
+ '(fci-rule-color "#073642")
+ '(foreign-regexp/regexp-type (quote perl))
+ '(help-at-pt-display-when-idle (quote (flymake-overlay)) nil (help-at-pt))
+ '(help-at-pt-timer-delay 0.3)
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
+ '(reb-re-syntax (quote foreign-regexp))
+ '(safe-local-variable-values (quote ((clmemo-mode . t))))
+ '(tool-bar-mode nil)
+ '(transient-mark-mode t)
+ '(yas-prompt-functions (quote (my-yas/prompt))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
