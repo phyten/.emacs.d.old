5f8cc20bfe8a326a48b586744a81a427aa2ee440
phyten <phyten.obr@gmail.com>
2013-11-19 20:54:33 +0900

    js2-modeのインデント数を2へと変更

--- inits/60_js2.el
+++ inits/60_js2.el
@@ -5,6 +5,12 @@
 (add-to-list 'auto-mode-alist '("\\.\\(js\\|json\\)$" . js2-mode))
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-
+(add-hook 'js2-mode-hook
+          #'(lambda ()
+              (require 'js)
+              (setq js-indent-level 2
+                    js-expr-indent-offset 2
+                    indent-tabs-mode nil)
+              (set (make-local-variable 'indent-line-function) 'js-indent-line)))
 
 (require 'nodejs-repl)
